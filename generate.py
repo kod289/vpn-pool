a2956f53650d9777ef24f89598709478026e2124
kod289 <oglocbaby1298@gmail.com>
2026-09-04 05:27:01 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from urllib.parse import urlsplit
 import base64
 import re
 
-INPUT = Path("output/quality.txt")
+INPUT = Path("output/reachable.txt")
 OUT = Path("output/subscriptions")
 
 OUT.mkdir(parents=True, exist_ok=True)
